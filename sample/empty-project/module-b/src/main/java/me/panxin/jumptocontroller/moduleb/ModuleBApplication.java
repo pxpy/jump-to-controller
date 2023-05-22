3b667f76ff229d587e3d3fe12548a16d93750cca
panpan <panxin@panxin.me>
2023-05-22 21:06:36 +0800

    修复demo

--- sample/empty-project/module-b/src/main/java/me/panxin/jumptocontroller/moduleb/ModuleBApplication.java
+++ sample/empty-project/module-b/src/main/java/me/panxin/jumptocontroller/moduleb/ModuleBApplication.java
@@ -2,7 +2,9 @@ package me.panxin.jumptocontroller.moduleb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients({"me.panxin.jumptocontroller"})
 @SpringBootApplication
 public class ModuleBApplication {
 
